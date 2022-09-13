4653915e230049970f2eb3493ae6337b715e07b4
Megan <meganmoulos@gmail.com>
2022-09-13 09:24:00 -0700

    fixed background, cards have some styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,13 +5,14 @@ function CharacterCard({character}) {
 
     return (
         <div className="card">
-            <h3 className='card-header-title'>{character.displayName}</h3>
-            <img src={character.sprite} alt={character.displayName}/>
-            <p className="information">Full Name:</p><span className="charInfo">{character.fullName}</span>
-            <p className="information">Species:</p><span className="charInfo">{character.species}</span>
-            <p className="information">Sex:</p><span className="charInfo">{character.sex}</span>
-            {quotes.map(quote => <p key={quote}>{quote}</p>)}
-            
+            <div>
+                <h3 className='card-header-title'>{character.displayName}</h3>
+                <img src={character.sprite} alt={character.displayName}/>
+                <p className="information">FULL NAME:</p><span className="charInfo">{character.fullName}</span>
+                <p className="information">SPECIES:</p><span className="charInfo">{character.species}</span>
+                <p className="information">SEX:</p><span className="charInfo">{character.sex}</span>
+                {quotes.map(quote => <p key={quote}>{quote}</p>)}
+            </div>
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,11 @@ function Home({characters}) {
     return (
         <div className="row">
             <div className="column">
-                {characters.map(character => (
-                    <CharacterCard key={character.id} character={character} />
-                ))}
+                <div>
+                    {characters.map(character => (
+                        <CharacterCard key={character.id} character={character} />
+                    ))}
+                </div>
             </div>
             <Footer />
         </div>
