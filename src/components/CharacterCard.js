b1457c35b0652b22b3502824924d53e04b324f63
Megan <meganmoulos@gmail.com>
2022-09-13 09:38:42 -0700

    tweaked card styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,14 +5,16 @@ function CharacterCard({character}) {
 
     return (
         <div className="card">
-            <div>
-                <h3 className='card-header-title'>{character.displayName}</h3>
-                <img src={character.sprite} alt={character.displayName}/>
-                <p className="information">FULL NAME:</p><span className="charInfo">{character.fullName}</span>
-                <p className="information">SPECIES:</p><span className="charInfo">{character.species}</span>
-                <p className="information">SEX:</p><span className="charInfo">{character.sex}</span>
-                {quotes.map(quote => <p key={quote}>{quote}</p>)}
-            </div>
+                <div className="card-left">
+                    <img src={character.sprite} alt={character.displayName}/>
+                </div>
+                <div className="card-right">
+                    <h3 className='card-header-title'>{character.displayName}</h3>
+                    <p className="information">FULL NAME:</p><span className="charInfo">{character.fullName}</span>
+                    <p className="information">SPECIES:</p><span className="charInfo">{character.species}</span>
+                    <p className="information">SEX:</p><span className="charInfo">{character.sex}</span>
+                    {quotes.map(quote => <p key={quote}>{quote}</p>)}
+                </div>
         </div>
     );
 }
