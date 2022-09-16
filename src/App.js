bc42e7f56b88dcc5595093dc0b5e96f77407ab6f
Teejay Dixon <tahjgdixon@gmail.com>
2022-09-16 10:00:11 -0700

    bug fix

--- src/App.js
+++ src/App.js
@@ -6,29 +6,7 @@ import AnimatedRoutes from './AnimatedRoutes';
 
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_URL}/characters`)
-    .then(res => {
-      setCharacters(res.data)
-    })
-    .catch(err => console.log(err))
-  }, []);
-
-const createCharacter = (newCharacter) => {
-  axios.post(`${process.env.REACT_APP_API_URL}/characters`, newCharacter)
-  .then(res => {
-    setCharacters(prevCharacters => [...prevCharacters, res.data])
-  })
-}
-
-const createComment = (newComment, id) => {
-  console.log(newComment, id)
-  axios.patch(`${process.env.REACT_APP_API_URL}/characters/${id}`, newComment)
-    .then(res => res.json())
-    .then(data => console.log(data))
-}
+  
 
   return (
     <>
